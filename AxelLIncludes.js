b2e3b51ac5ea4ce598eca5412d744ac73871a096
unknown <20233tn154@utez.edu.mx>
2024-06-03 21:18:05 -0600

    Investigación de array methods

---
+++ AxelLIncludes.js
@@ -0,0 +1,6 @@
+let palabras = ['pera', 'uva', 'naranja', 'pera', 'manzana', 'sandia'];
+palabras.includes('naranja') // true
+palabras.includes('melon') // false
+palabras.includes('manzana') // true
+
+
